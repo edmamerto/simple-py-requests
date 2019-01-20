143681947b53bd5871a264d261d232a3f37231a0
Ed Mamerto <edmamerto@gmail.com>
2019-01-20 16:40:46 -0500

    fix code styling to comply with PEP8

--- requests_sample.py
+++ requests_sample.py
@@ -1,52 +1,54 @@
 """
 @author Ed Mamerto
 """
-import requests
 import json
+import requests
 
-gender_stats = {
-	'male': 0,
-	'female': 0
+GENDER_STATS = {
+    'male': 0,
+    'female': 0
 }
 
-def check_gender(name):
-	""" 
-    Checks if name is a male or female name 
-  
-    Parameters: 
-    name (list): first name (index 0),  last name (index 2)
-  
-    Returns: 
-    string: 'male' or 'female'
-  
-    """
-	r = requests.get('https://api.namsor.com/onomastics/api/json/gender/'+name[0]+'/'+name[1])
-	obj = json.loads(r.text)
 
-	return obj['gender']
+def check_gender(name):
+    """
+Checks if name is a male or female name
+
+Parameters:
+name (list): first name (index 0),  last name (index 2)
+
+Returns:
+string: 'male' or 'female'
+
+"""
+    r = requests.get(
+        'https://api.namsor.com/onomastics/api/json/gender/' +
+        name[0] +
+        '/' +
+        name[1])
+    obj = json.loads(r.text)
+
+    return obj['gender']
+
 
 def count_genders():
-	""" 
-    counts how many users are male and female 
-  
-    Returns: 
-    string: gender stats
-    """	
-	r = requests.get('https://jsonplaceholder.typicode.com/users')
-	users = json.loads(r.text)
+    """
+counts how many users are male and female
 
-	for user in users:
-		# split name on white space to save first and last name in an array
-		name = user['name'].split()
-		gender_stats[check_gender(name)]+=1 
+Returns:
+string: gender stats
+"""
+    r = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = json.loads(r.text)
 
-	return 'there are ' + str(gender_stats['male']) + ' males and ' + str(gender_stats['female']) + ' females'
+    for user in users:
+        # split name on white space to save first and last name in an array
+        name = user['name'].split()
+        GENDER_STATS[check_gender(name)] += 1
 
-
-if __name__== "__main__":
-  print count_genders()
+    return 'there are ' + \
+        str(GENDER_STATS['male']) + ' males and ' + str(GENDER_STATS['female']) + ' females'
 
 
-
-
-
+if __name__ == "__main__":
+    print count_genders()
